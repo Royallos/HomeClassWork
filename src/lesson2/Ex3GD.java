7281cbcd01c33244ab5eeb87e6e58936dca82178
Alexandr Rallo <apbyz@tut.by>
2020-03-23 10:03:27 +0300

    Lesson2 is ready

---
+++ src/lesson2/Ex3GD.java
@@ -0,0 +1,38 @@
+package lesson2;
+import java.util.Scanner;
+
+//        3) Дано целое число. Если оно является положительным, то прибавить к нему 1.
+//        Если отрицательным, то вычесть из него 2. Если нулевым, то заменить его на10.
+//        Вывести полученное число
+
+public class Ex3GD {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your number:");
+
+        int n = scan.nextInt();
+
+        if (n > 0) {
+
+            n += 1;
+
+        } else if (n < 0){
+
+            n -= 2;
+
+//        } else if (n == 0) {
+
+//            тут if не нужен - это единственно возможный оставшийся вариант (c)
+
+        } else {
+
+            n +=10;
+        }
+
+        System.out.println("" + n);
+
+    }
+
+}
---
+++ src/lesson2/Ex4.java
@@ -0,0 +1,25 @@
+package lesson2;
+
+//        Задание 4
+//        Определить число, полученное выписыванием в обратном порядке цифр
+//        любого 4-х значного натурального числа n.
+
+public class Ex4 {
+
+    public static void main (String [] args) {
+
+        int n = 5236;
+
+        int z = n % 10;
+        int a = n - z;
+        int y = (a % 100)/10;
+        int b = a - y;
+        int x = (b % 1000) / 100;
+        int c = b - x;
+        int w = (c % 10000) / 1000;
+
+        System.out.println("Number is " + z + y + x + w);
+
+    }
+
+}
